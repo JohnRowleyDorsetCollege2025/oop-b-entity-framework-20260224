122e7870f585e07f63aa9096ec21d968df45bfc6
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2026-02-24 11:58:27 +0000

    Various

--- OrderPortal.Api/Data/AppDbContext.cs
+++ OrderPortal.Api/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.EntityFrameworkCore;
 using OrderPortal.Domain.Models;
 
 namespace OrderPortal.Api.Data;
--- OrderPortal.Api/Program.cs
+++ OrderPortal.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OrderPortal.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var cs = builder.Configuration.GetConnectionString("MySql");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(cs, ServerVersion.AutoDetect(cs));
+});
+
 
 var app = builder.Build();
 
